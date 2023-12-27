877b252e33785f1b984c12410c94999d4ad4b35a
M S I <ghani.zumar.ft21@mail.umy.ac.id>
2023-12-27 16:59:37 +0700

    add isi function TopAppBarKontak

--- app/src/main/java/com/example/consumerestapi/ui/theme/theme/KontakApp.kt
+++ app/src/main/java/com/example/consumerestapi/ui/theme/theme/KontakApp.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -23,17 +24,17 @@ import com.example.consumerestapi.ui.theme.home.viewmodel.HomeViewModel
 @Composable
 fun KontakApp(
     homeViewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){
+) {
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
-    Scaffold (
+    Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = { TopAppBar(scrollBehavior = scrollBehavior)}
-    ){
-        Surface (
+        topBar = { TopAppBar(scrollBehavior = scrollBehavior) }
+    ) {
+        Surface(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
-        ){
+        ) {
             HomeScreen(
                 kontakUIState = homeViewModel.kontakUIState,
                 retryAction = homeViewModel::getKontak
@@ -50,11 +51,22 @@ fun TopAppBarKontak(
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
-){}
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+
+                }
+            }
+        })
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier){
+fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
     CenterAlignedTopAppBar(
         scrollBehavior = scrollBehavior,
         title = {
