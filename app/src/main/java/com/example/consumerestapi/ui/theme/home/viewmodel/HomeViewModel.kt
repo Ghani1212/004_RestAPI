4afc17d5aedd49727cfce85620b058a5f778d085
M S I <ghani.zumar.ft21@mail.umy.ac.id>
2023-12-27 18:05:04 +0700

    add function deletekontak

--- app/src/main/java/com/example/consumerestapi/ui/theme/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/theme/home/viewmodel/HomeViewModel.kt
@@ -11,13 +11,13 @@ import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
 
-sealed class KontakUIState{
-    data class  Success(val kontak: List<Kontak>) : KontakUIState()
+sealed class KontakUIState {
+    data class Success(val kontak: List<Kontak>) : KontakUIState()
     object Error : KontakUIState()
     object Loading : KontakUIState()
 }
 
-class HomeViewModel (private val kontakRepository: KontakRepository) : ViewModel(){
+class HomeViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
     var kontakUIState: KontakUIState by mutableStateOf(KontakUIState.Loading)
         private set
 
@@ -25,14 +25,26 @@ class HomeViewModel (private val kontakRepository: KontakRepository) : ViewModel
         getKontak()
     }
 
-    fun getKontak(){
+    fun getKontak() {
         viewModelScope.launch {
             kontakUIState = KontakUIState.Loading
             kontakUIState = try {
                 KontakUIState.Success(kontakRepository.getKontak())
-            } catch (e: IOException){
+            } catch (e: IOException) {
                 KontakUIState.Error
-            } catch (e: HttpException){
+            } catch (e: HttpException) {
+                KontakUIState.Error
+            }
+        }
+    }
+
+    fun deleteKontak(id: Int) {
+        viewModelScope.launch {
+            try {
+                kontakRepository.deleteKontak(id)
+            } catch (e: IOException) {
+                KontakUIState.Error
+            } catch (e: IOException) {
                 KontakUIState.Error
             }
         }
